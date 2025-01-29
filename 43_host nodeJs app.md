10ebfeade26dc85325748ce499fa322f34624d5f
Teertharaj Turkar <teertharajturkar@gmail.com>
2025-01-29 21:43:23 +0530

    the project  completed

---
+++ 43_host nodeJs app.md
@@ -0,0 +1,41 @@
+****Host Your Node.js App for FREE ****
+-------------------------------------------------------------
+i Want to host your Node.js app for free? In this video, I’ll show you how to deploy your Node app using Render.com and GitHub, all while keeping costs at zero! We'll also cover setting up package.json, adding scripts, and reviewing Hostinger as an option. Perfect for beginners and developers looking to deploy projects quickly and easily. 💸
+
+
+==============================================
+{
+  "name": "url_shortener_mega_project",
+  "version": "1.0.0",
+  "description": "this is URL Shortener project",
+  "main": "app.js",
+  "type":"module",
+  "scripts": {
+    "dev":"node --watch app.js",
+    "start":"node app.js"
+  },
+  "keywords": [],
+  "author": "teertharaj turkar",
+  "license": "ISC"
+}
+
+============================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,100 @@
+import { readFile } from "fs/promises";
+import { createServer } from "http";
+import path, { join } from "path";
+import crypto from "crypto";
+import { writeFile } from "fs/promises";
+import { json } from "stream/consumers";
+import { link } from "fs/promises";
+
+const PORT = 3002;
+
+const DATA_FILE = path.join("data", "links.json");
+
+//serve file in server
+const serveFile = async (res, filePath, contentType) => {
+  try {
+    const data = await readFile(filePath);
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  } catch (error) {
+    res.writeHead(404, { "Content-Type": "contant/plain" });
+    res.end("404 page not found");
+  }
+};
+
+const loadLinks = async () => {
+  try {
+    const data = await readFile(DATA_FILE, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await writeFile(DATA_FILE, JSON.stringify({}));
+      return {};
+    }
+    throw error;
+  }
+};
+
+//save links
+const saveLinks = async (links) => {
+  await writeFile(DATA_FILE, JSON.stringify(links));
+};
+
+// our server
+const server = createServer(async (req, res) => {
+  console.log(req.url);
+
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      return serveFile(res, path.join("public", "index.html"), "text/html");
+    } else if (req.url === "/style.css") {
+      return serveFile(res, path.join("public", "style.css"), "text/css");
+    } else if (req.url === "/links") {
+      const links = await loadLinks();
+      res.writeHead(200, { "content-type": "application/json" });
+      return res.end(JSON.stringify(links));
+    } else {
+      const links = await loadLinks();
+      const shortCode = req.url.slice(1);
+      console.log("link redirect", req.url);
+      if (links[shortCode]) {
+        res.writeHead(302, { location: links[shortCode] });
+        return res.end();
+      }
+
+      res.writeHead(404, { "content-type": "application/json" });
+      return res.end("shortend URL is not found")
+    }
+  }
+
+  if ((req.mathod === "POST", req.url === "/shorten")) {
+    const links = await loadLinks();
+
+    let body = "";
+    req.on("data", (chunk) => (body += chunk));
+    req.on("end", async () => {
+      console.log(body);
+      const { url, shortCode } = JSON.parse(body);
+      if (!url) {
+        res.writeHead(400, { "content-type": "text/plain" });
+        return res.end("URL Is required");
+      }
+
+      const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+      if (links[finalShortCode]) {
+        res.writeHead(400, { "content-type": "text/plain" });
+        return res.end("Short code already exists.Please choose another");
+      }
+
+      links[finalShortCode] = url;
+      await saveLinks(links);
+
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ success: true, shortCode: finalShortCode }));
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
